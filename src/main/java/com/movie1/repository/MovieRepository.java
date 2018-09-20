2ebc9e9decbe438fcdc0a1d974d477474e410493
haha <14301133@bjtu.edu.cn>
2018-09-20 23:20:09 +0800

    修改电影接口返回10条按热度排序

--- src/main/java/com/movie1/repository/MovieRepository.java
+++ src/main/java/com/movie1/repository/MovieRepository.java
@@ -21,7 +21,7 @@ public interface MovieRepository extends JpaRepository<Movie, Integer> {
      * @param moviename
      * @return
      */
-    List<Movie> findFirst5ByMovienameLike(String moviename);
+    List<Movie> findFirst10ByMovienameLikeOrOrderByNationDesc(String moviename);
 
     /**
      *
